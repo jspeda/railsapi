42050a59b5fc7cb0a99fb94bf81a030a7dd03b36
Joe <jspedale@gmail.com>
2017-06-21 03:41:31 +0000

    Added delete functionality with tests.

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -86,5 +86,20 @@ RSpec.describe NotesController, type: :controller do
             expect(json["errors"]["title"][0]).to eq("can't be blank")
         end
     end
+    
+    describe "notes#destroy action" do
+        it "should destroy a saved note" do
+            note = FactoryGirl.create(:note)
+            delete :destroy, params: { id: note.id }
+            note = Note.find_by_id(note.id)
+            expect(note).to eq nil
+        end
+        
+        it "should return no_content status" do
+            note = FactoryGirl.create(:note)
+            delete :destroy, params: { id: note.id }
+            expect(response).to have_http_status(:no_content)
+        end
+    end
 
 end
