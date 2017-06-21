b5ea9462a8356a7673ac7c8fa6f71dd3b6458e8e
Joe <jspedale@gmail.com>
2017-06-21 01:54:16 +0000

    Added more tests, added creation action to the notes controller.

---
+++ spec/controllers/notes_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe NotesController, type: :controller do
+    describe "notes#index action" do
+        it "should successfully respond" do
+            get :index
+            expect(response).to be_success
+        end
+    end
+    
+    describe "notes#create action" do 
+        before do
+            post :create, params: { note: { title: 'First', content: 'Hello' } }
+        end
+        it "should return 200 status-code" do
+            expect(response). to be_success
+        end
+        
+        it "should create/save a new note in the db" do
+            note = Note.last
+            expect(note.content).to eq('Hello')
+            expect(note.title).to eq('First')
+        end
+        
+        it "should return the created note in response body" do
+            json = JSON.parse(response.body)
+            expect(json['content']).to eq('Hello')
+            expect(json['title']).to eq('First')
+        end
+    end
+
+end
